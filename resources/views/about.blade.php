3c0098845bf0380923c08f8fa60954f2901f3686
Rasyaa21 <naufalrasya21907@gmail.com>
2024-10-23 14:34:37 +0700

    commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>About</title>
+    @vite('resources/css/app.css')
+</head
+<body class="bg-blue-400">
+    <div class="flex justify-center">
+        <h1 class="text-2xl text-pink-400">About</h1>
+    </div>
+</body>
+</html>
